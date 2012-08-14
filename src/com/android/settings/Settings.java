6045a2dbe515c7324e42d3cd275c3b4c9d301aae
Eagle1967 <icsourcery@gmail.com>
2012-08-13 22:51:00 -0600

    APN settings fix -CM--must change phone/xml/gsm_umts_options.xml line 27 Update reference to Apn Settings  android:targetClass=com.android.settings.Settings$ApnSettingsActivity

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -787,4 +787,6 @@ public class Settings extends PreferenceActivity
     public static class AdvancedWifiSettingsActivity extends Settings { /* empty */ }
     public static class TextToSpeechSettingsActivity extends Settings { /* empty */ }
     public static class AndroidBeamSettingsActivity extends Settings { /* empty */ }
+    public static class ApnSettingsActivity extends Settings { /* empty */ }
+    public static class ApnEditorActivity extends Settings { /* empty */ }
 }
