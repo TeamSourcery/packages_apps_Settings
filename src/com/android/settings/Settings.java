90add1d1f8aef3ef334e3ff7e09fb07436601f4a
eagle1967 <icsourcery@gmail.com>
2013-05-17 01:09:09 -0700

    adjustments for trds feature

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -855,4 +855,5 @@ public class Settings extends PreferenceActivity
     public static class AndroidBeamSettingsActivity extends Settings { /* empty */ }
     public static class WifiDisplaySettingsActivity extends Settings { /* empty */ }
     public static class DreamSettingsActivity extends Settings { /* empty */ }
+    public static class QuietHoursSettingsActivity extends Settings { /* empty */ }
 }
